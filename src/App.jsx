f867cfb30454c08ea20763eb2d9cbd239fb56191
ZainX38 <zainchema2007@gmail.com>
2026-01-27 20:23:46 +0000

    Refactor Hero section: enhance layout for responsiveness in phones

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import Hero from './components/sections/Hero.jsx'
 
 function App() {
   return (
-    <main className="bg-linear-to-b from-black via-sky-700 to-gray-800 
-    min-h-screen text-white px-60 py-20 font-sans">
-      <Hero />
+    <main className="
+    bg-linear-to-b from-black via-sky-700 to-gray-800 
+    text-white font-sans 
+    px-18 sm:px-20 md:px-28 lg:px-60 py-20
+    max-w-screen min-w-xl min-h-dvh
+    ">
+      <Hero/>
     </main>
     
   )
--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -12,19 +12,19 @@ function Hero() {
         }];
 
     return (
-        <section id="hero">
+        <section id="hero" className='max-w-5xl mx-auto px-4'>
             <div className="flex flex-row items-center gap-10 mb-4">
                 <img src={heroImage} alt="Hero Image" className="
-                w-20 h-20 object-cover rounded-full
-                "></img>
+                w-14 h-14 sm:w-20 sm:h-20 object-cover rounded-full"
+                ></img>
                 <a href="#" className="rounded-4xl border border-white px-2 py-1 text-xs">
                     Available for internships
                 </a>
             </div>
 
-            <h1 className="text-4xl font-bold mb-4">Hey, I'm Zain</h1>
+            <h1 className="text-xl sm:text-3xl font-bold leading-tight mb-4">Hey, I'm Zain</h1>
 
-            <p className="text-lg max-w-3xl mb-4">
+            <p className="text-sm md:text-lg min-w-44 mb-4 text-balance">
                 I’m a first-year Computer Science student building <span className='font-bold text-amber-300'> full-stack web applications and exploring AI-powered solutions </span>to solve real problems.
                 I’m actively seeking software engineering internships where I can contribute, learn, and grow.
             </p>
@@ -36,7 +36,7 @@ function Hero() {
                         key={button.name} 
                         href={button.link} 
                         className="
-                        text-md px-2 py-1 border rounded-full cursor-pointer bg-sky-900
+                        text-sm md:text-md px-2 py-1 border rounded-full cursor-pointer bg-sky-900
                         hover:bg-white hover:text-black hover:scale-110
                         transition-transform flex flex-row items-center gap-2">
                             <svg aria-hidden="true" 
