633c8464507c0ff415b5bb3072e22d03cb7d8b27
ZainX38 <zainchema2007@gmail.com>
2026-01-27 22:38:35 +0000

    Refractor Hero to create a glowing effect

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -17,9 +17,20 @@ function Hero() {
                 <img src={heroImage} alt="Hero Image" className="
                 w-14 h-14 sm:w-20 sm:h-20 object-cover rounded-full"
                 ></img>
-                <a href="#" className="rounded-4xl border border-white px-2 py-1 text-xs">
-                    Available for internships
-                </a>
+
+                <div 
+                className="relative overflow-hidden rounded-full p-px text-xs">
+                    {/*Creates Animation with moving gradient*/}
+                    <span className="
+                    absolute inset-[-1000%] animate-[spin_2s_linear_infinite]
+                    bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"/>
+                    {/*MASK - covers centre so only border is shown*/}
+                    <span className='
+                    inline-flex h-full w-full items-center justify-center rounded-full
+                    bg-slate-950 px-3 py-1 font-medium text-white backdrop-blur-3xl'>
+                        Available for internships
+                    </span>
+                </div>
             </div>
 
             <h1 className="text-xl sm:text-3xl font-bold leading-tight mb-4">Hey, I'm Zain</h1>
